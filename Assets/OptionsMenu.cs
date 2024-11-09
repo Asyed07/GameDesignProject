24739d64326e4d491298d691c9f9843206193342
Asyed07 <18alir@loreto.herts.sch.uk>
2024-11-09 10:36:48 +0000

    Project setup

---
+++ Assets/OptionsMenu.cs
@@ -0,0 +1,12 @@
+using TMPro.Examples;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+   public void SetVolume (float volume)
+   {
+        audioMixer.SetFloat("volume", volume);
+   }
+}
