5cf828b40905085a349aadbbdbdfa072aadebd66
Asyed07 <18alir@loreto.herts.sch.uk>
2024-11-15 12:59:03 +0000

    Backup before lighting

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void GoToScene(string sceneName)
+    public void GoToScene(string sceneName) // Loads the scene selected in unity
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    public void QuitApp()
+    public void QuitApp() // Player can close the application
     {
         Application.Quit();
         Debug.Log("Application has quit.");
