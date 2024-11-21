5f626711678ff556befb05f0b657cd656c06f083
Asyed07 <18alir@loreto.herts.sch.uk>
2024-11-21 10:55:56 +0000

    GUI 1.1

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,24 +8,24 @@ public class PauseMenu : MonoBehaviour
     public void Pause()
     {
          pauseMenu.SetActive(true);
-        Time.timeScale = 0;
+        Time.timeScale = 0; // Pauses the game while the pause menu is open
     }
 
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
-        Time.timeScale = 1;
+        Time.timeScale = 1; // Unpauses the game while the pause menu is open
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
-        Time.timeScale = 1;
+        Time.timeScale = 1; // Unpauses the game while the pause menu is open
     }
 
     
     public void Restart(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName);  // Restarts the level selected
     }
 }
