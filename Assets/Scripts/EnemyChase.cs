74e849b99b9282bf33626b24f1a967ff0eb2c626
Asyed07 <18alir@loreto.herts.sch.uk>
2025-01-07 20:22:01 +0000

    s

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -2,33 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This script allows an enemy object to chase a player object with some constraints.
 public class EnemyChase : MonoBehaviour
 {
-    public GameObject player;
-    public float speed;
-    private float distance;
-    public float distanceBetween; 
-    
+    public GameObject player; // Reference to the player GameObject that the enemy will chase.
+    public float speed; // Speed at which the enemy moves towards the player.
+    private float distance; // Distance between the enemy and the player.
+    public float distanceFromPlayer; // Minimum distance the enemy maintains from the player.
+
     void Update()
     {
+        // Calculate the distance between the enemy and the player.
         distance = Vector2.Distance(transform.position, player.transform.position);
+
+        // Calculate the direction vector from the enemy to the player.
         Vector2 direction = player.transform.position - transform.position;
         direction.Normalize();
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
-        if (distance > distanceBetween)
+        // Move the enemy towards the player if it's farther than the specified distance.
+        if (distance > distanceFromPlayer)
         {
-            transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
 
-        if (distance >= 0.01f)
+        // Determine whether the enemy should face left or right.
+        if (direction.x > 0) // Player is to the right of the enemy.
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            transform.localScale = new Vector3(-3f, 3f, 1f); // Face right.
         }
-        else if (distance <= -0.01f)
+        else if (direction.x < 0) // Player is to the left of the enemy.
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            transform.localScale = new Vector3(3f, 3f, 1f); // Face left.
         }
     }
 }
